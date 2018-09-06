532924fdc491c15314cb5a93a589de1c20c8fdc3
Christian Gerlach <christianrgerlach@gmail.com>
2018-09-05 23:18:19 -0700

    pre-database

--- webmiko_app/routes.py
+++ webmiko_app/routes.py
@@ -1,11 +1,35 @@
 from threading import Lock
-from flask import render_template
-from webmiko_app import app
-from webmiko_app import socketio
+from flask import render_template, send_from_directory
+from webmiko_app import app, socketio
 from flask_socketio import emit, join_room, leave_room, close_room, rooms, disconnect
 
 thread = None
 thread_lock = Lock()
+
+
+
+
+@socketio.on('query', namespace='/test')
+def test_message(message):
+    print('Query sent!')
+    emit('my_response', {'data': message['data'], 'count': 123})
+
+@socketio.on('config', namespace='/test')
+def test_message(message):
+    print('Config sent!')
+    emit('my_response', {'data': message['data'], 'count': 123})
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    print('Client connected')
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+
 
 def background_thread():
     """Example of how to send server generated events to clients."""
@@ -25,16 +49,26 @@ def start_background_thread():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.j2')
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
-    emit('my_response', {'data': message['data'], 'count': 123})
+@app.route('/<path:path>')
+def serve_static(path):
+    print(app.root_path)
+    send_from_directory('static',path)
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my_response', {'data': 'Connected', 'count': 0})
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
