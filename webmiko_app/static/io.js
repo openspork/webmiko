2da48f12a297d1f6e0501b472723eadb34b22b1b
Christian Gerlach <christianrgerlach@gmail.com>
2018-09-14 15:46:24 -0700

    split up js files

--- webmiko_app/static/io.js
+++ webmiko_app/static/io.js
@@ -1,20 +1,3 @@
-data = [
-{title: "All Assets", folder: true, children: [
-        {title: "sw1_24p", folder: true, children: [
-            {title: "$password=P@ss1234"},
-            {title: "$user=manager"},
-            {title: "$ip=10.0.0.10"},
-            {title: "$trunk1=1,24"},
-        ]},
-        {title: "sw2_48p", folder: true, children: [
-            {title: "$password=P@ss1234"},
-            {title: "$user=manager"},
-            {title: "$ip=10.0.0.11"},
-            {title: "$trunk=47-48"}
-        ]},
-    ]}
-]
-
 function genGuid() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
@@ -84,10 +67,6 @@ $(document).ready(function() {
         $(`#${guid}_result`).text(result);
     });
 
-
-
-
-
     $('#config_button').click(function(event) {
         if ($('#console_input').val()) {
             if (confirm('Continue with config?')) {
@@ -107,30 +86,5 @@ $(document).ready(function() {
 
     socket.on('inventory', function(msg) {
         alert('Inventory update: ' + msg.data)
-    });
-
-    $("#tree").fancytree({
-        minExpandLevel: 2,
-        checkbox: true,
-        source: data,
-        extensions: ["edit"],
-        edit: {
-        // Available options with their default:
-        adjustWidthOfs: 4,   // null: don't adjust input size to content
-        inputCss: { minWidth: "3em" },
-        triggerStart: ["clickActive", "f2", "dblclick", "shift+click", "mac+enter"],
-        beforeEdit: $.noop,   // Return false to prevent edit mode
-        edit: $.noop,         // Editor was opened (available as data.input)
-        beforeClose: $.noop,  // Return false to prevent cancel/save (data.input is available)
-        save: $.noop,         // Save data.input.val() or return false to keep editor open
-        close: $.noop,        // Editor was removed
-    },
-    select: function(event, data) {
-        var node = data.node;
-        selected = node.isSelected(); // Get the selected state of the selected node
-            node.visit(function(childNode) {
-                childNode.setSelected(selected); // apply the selected state to all children
-            });
-    }
     });
 });
---
+++ webmiko_app/static/tree.js
@@ -0,0 +1,43 @@
+data = [
+{title: "All Assets", folder: true, children: [
+        {title: "sw1_24p", folder: true, children: [
+            {title: "$password=P@ss1234"},
+            {title: "$user=manager"},
+            {title: "$ip=10.0.0.10"},
+            {title: "$trunk1=1,24"},
+        ]},
+        {title: "sw2_48p", folder: true, children: [
+            {title: "$password=P@ss1234"},
+            {title: "$user=manager"},
+            {title: "$ip=10.0.0.11"},
+            {title: "$trunk=47-48"}
+        ]},
+    ]}
+]
+
+$(document).ready(function() {
+    $("#tree").fancytree({
+        minExpandLevel: 2,
+        checkbox: true,
+        source: data,
+        extensions: ["edit"],
+        edit: {
+        // Available options with their default:
+        adjustWidthOfs: 4,   // null: don't adjust input size to content
+        inputCss: { minWidth: "3em" },
+        triggerStart: ["clickActive", "f2", "dblclick", "shift+click", "mac+enter"],
+        beforeEdit: $.noop,   // Return false to prevent edit mode
+        edit: $.noop,         // Editor was opened (available as data.input)
+        beforeClose: $.noop,  // Return false to prevent cancel/save (data.input is available)
+        save: $.noop,         // Save data.input.val() or return false to keep editor open
+        close: $.noop,        // Editor was removed
+    },
+    select: function(event, data) {
+        var node = data.node;
+        selected = node.isSelected(); // Get the selected state of the selected node
+            node.visit(function(childNode) {
+                childNode.setSelected(selected); // apply the selected state to all children
+            });
+    }
+    });
+});
