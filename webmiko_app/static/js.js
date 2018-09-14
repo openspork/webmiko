e45ffee75397f2e3bc2481985ad88fa3fdd8df79
Christian Gerlach <christianrgerlach@gmail.com>
2018-09-13 23:07:52 -0700

    adding guid gen back

--- webmiko_app/static/js.js
+++ webmiko_app/static/js.js
@@ -15,6 +15,15 @@ data = [
     ]}
 ]
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
 String.prototype.hashCode = function(){
     var hash = 0;
     if (this.length == 0) return hash;
